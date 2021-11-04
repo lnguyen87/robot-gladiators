d517f6bbfe5a2d52cb661ed6c91e802cb059f841
lnguyen87 <luc_nguyen@outlook.com>
2021-11-04 14:25:36 -0600

    fixed bug issue with player-name left blank

--- assets/js/game.js
+++ assets/js/game.js
@@ -79,7 +79,7 @@ var startGame = function() {
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-
+      
       // pick new enemy to fight based on the index of the enemy.names array
       var pickedEnemyObj = enemyInfo[i];
 
@@ -166,8 +166,24 @@ var startGame = function() {
     }
   };
 
+
+  // function to set name
+  var getPlayerName =function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+      name = prompt("What is your robot's name?");
+    }
+    console.log("Your robot's name is " + name);
+    return name;
+  };
+
+  /* END GAME FUNCTIONS */
+
+  /* GAME INFORMATION / VARIABLES */
+
   var playerInfo = {
-    name: window.prompt("what is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
